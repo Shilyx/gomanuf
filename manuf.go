a3e0b7acaefc9ea8cacfa0565870b98a54d30c05
shilyx <shilyx@outlook.com>
2021-01-30 17:39:43 +0800

    去panic

--- manuf.go
+++ manuf.go
@@ -8,16 +8,76 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/kardianos/osext"
 )
 
 const hexDigit = "0123456789ABCDEF"
 
-var d map[int]interface{}
+var dGlobal map[int]interface{} = nil
+var m sync.Mutex
 
-func init() {
-	d = make(map[int]interface{})
+// fileExists checks if a file exists and is not a directory before we
+// try using it to prevent further errors.
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func b2uint64(sList []string) uint64 {
+	var t uint64
+	for i, b := range sList {
+		l := strings.Index(hexDigit, string(b[0]))
+		r := strings.Index(hexDigit, string(b[1]))
+		t += uint64((l<<4)+r) << uint8((6-i-1)*8)
+	}
+
+	return t
+}
+
+func initMapInternal() (map[int]interface{}, error) {
+	ret := make(map[int]interface{})
+
+	parse := func(mac, comment string) {
+		g := strings.Split(mac, "/")
+		m := strings.Split(g[0], ":")
+		var b int
+		if len(g) != 2 {
+			b = 48 - len(m)*8
+		} else {
+			b, _ = strconv.Atoi(g[1])
+		}
+		if _, ok := ret[b]; !ok {
+			ret[b] = make(map[uint64]string)
+
+		}
+		ret[b].(map[uint64]string)[b2uint64(m)] = comment
+	}
+
+	readLine := func(fileName string, handler func(string)) error {
+		f, err := os.Open(fileName)
+		defer f.Close()
+		if err != nil {
+			return err
+		}
+		buf := bufio.NewReader(f)
+		for {
+			line, err := buf.ReadString('\n')
+			line = strings.TrimSpace(line)
+			handler(line)
+			if err != nil {
+				if err == io.EOF {
+					return nil
+				}
+				return err
+			}
+		}
+	}
+
 	f := "manuf"
 
 	if !fileExists(f) {
@@ -49,49 +109,33 @@ func init() {
 		}
 	})
 
-	if err != nil {
-		panic(err)
-	}
+	return ret, err
 }
 
-// fileExists checks if a file exists and is not a directory before we
-// try using it to prevent further errors.
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
-}
-
-func parse(mac, comment string) {
-	g := strings.Split(mac, "/")
-	m := strings.Split(g[0], ":")
-	var b int
-	if len(g) != 2 {
-		b = 48 - len(m)*8
-	} else {
-		b, _ = strconv.Atoi(g[1])
-	}
-	if _, ok := d[b]; !ok {
-		d[b] = make(map[uint64]string)
-
-	}
-	d[b].(map[uint64]string)[b2uint64(m)] = comment
-}
-
-func b2uint64(sList []string) uint64 {
-	var t uint64
-	for i, b := range sList {
-		l := strings.Index(hexDigit, string(b[0]))
-		r := strings.Index(hexDigit, string(b[1]))
-		t += uint64((l<<4)+r) << uint8((6-i-1)*8)
+func initMap() error {
+	if dGlobal != nil {
+		return nil
 	}
 
-	return t
+	m.Lock()
+	defer m.Unlock()
+
+	if dGlobal == nil {
+		if d, e := initMapInternal(); e == nil {
+			dGlobal = d
+		} else {
+			return e
+		}
+	}
+
+	return nil
 }
 
 func Search(mac string) string {
+	if err := initMap(); err != nil {
+		return "failed"
+	}
+
 	mac = strings.ToUpper(mac)
 	mac = strings.Replace(mac, "-", ":", -1)
 	mac = strings.Replace(mac, " ", "", -1)
@@ -121,33 +165,12 @@ func Search(mac string) string {
 	}
 
 	bint := b2uint64(s)
-	for b := range d {
+	for b := range dGlobal {
 		k := 48 - b
 		bint = (bint >> uint8(k)) << uint8(k)
-		if _, ok := d[b].(map[uint64]string)[bint]; ok {
-			return d[b].(map[uint64]string)[bint]
+		if _, ok := dGlobal[b].(map[uint64]string)[bint]; ok {
+			return dGlobal[b].(map[uint64]string)[bint]
 		}
 	}
 	return ""
-}
-
-func readLine(fileName string, handler func(string)) error {
-	f, err := os.Open(fileName)
-	defer f.Close()
-	if err != nil {
-		return err
-	}
-	buf := bufio.NewReader(f)
-	for {
-		line, err := buf.ReadString('\n')
-		line = strings.TrimSpace(line)
-		handler(line)
-		if err != nil {
-			if err == io.EOF {
-				return nil
-			}
-			return err
-		}
-	}
-	return nil
 }
